15dc43bc522c8610c65932df40de1c169fbe345d
Marcelo Bresani <marcelobresani@gmail.com>
2022-04-19 19:05:24 -0400

    Add Empty Apps

---
+++ caja/urls.py
@@ -0,0 +1,10 @@
+# from django.conf.urls import url
+import caja.views
+
+urlpatterns = [
+    # url(
+    #     r'empresa/(?P<empresa>[0-9]+)/areas_moderadas/$',
+    #     empresa.views.AreaModerarList.as_view(),
+    #     name='areas-moderar-list'
+    # ),
+]
